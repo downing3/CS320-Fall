4c7a98733d2f5eb099bad12d2ede7bb56ed08ffa
Naomi Downing <downing3@bu.edu>
2023-09-19 13:01:57 -0400

    fixed string_get bug

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -1,10 +1,13 @@
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
 let string_init = String.init
 let string_length = String.length
-let string_get = String.get
+let string_get(cs, i0) = String.get cs i0
 
 let stringrev(cs: string): string = 
   let str_len = string_length cs in
-  string_init str_len (fun i -> string_get cs (str_len - 1 - i))
+  string_init str_len (fun i -> string_get (cs, (str_len - 1 - i)))
 
 (* ****** ****** *)
 (*
