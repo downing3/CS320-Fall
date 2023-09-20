4177a919bdc526dfc2d19d3ef0929c2902f223ba
Naomi Downing <downing3@bu.edu>
2023-09-20 18:49:16 -0400

    fixed errors

--- assigns/assign1/MySolution/assign1-4.ml
+++ assigns/assign1/MySolution/assign1-4.ml
@@ -1,29 +1,38 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
-let intrep_add(ds1: string)(ds2: string): string =
+let intrep_add (ds1: string) (ds2: string): string =
 
     let add int1 int2 carry = 
         let sum = int1 + int2 + carry in
         (sum mod 10, sum / 10) 
     in
 
-    let rec iterate s1 s2 i carry fwork =
-        let len1 = string_length s1 in
-        let len2 = string_length s2 in
+    let string_of_result fwork = 
+        let len1 = String.length ds1 in
+        let len2 = String.length ds2 in
         let max_len = max len1 len2 in
 
-        if i >= max_len then
-            if carry = 1 then fwork (fun append -> append '1')
-            else ()
-        else
-            let char1 = if i < len1 then string_get_at s1 (len1 - 1 - i) else '0' in
-            let char2 = if i < len2 then string_get_at s2 (len2 - 1 - i) else '0' in
-            let num1 = digit_of_char char1 in
-            let num2 = digit_of_char char2 in
-            let (current_sum, new_carry) = add num1 num2 carry in
+        let rec process_list chars =
+            match chars with
+            | [] -> ()
+            | h :: t -> fwork h; process_list t
+        in
 
-            fwork (fun append -> append (char_of_int (current_sum + int_of_char '0')));
-            iterate s1 s2 (i + 1) new_carry fwork
+        let rec iterate i carry acc = 
+            if i < max_len || carry != 0 then
+                let char1 = if (len1 - 1 - i) >= 0 then string_get_at ds1 (len1 - 1 - i) else '0' in
+                let char2 = if (len2 - 1 - i) >= 0 then string_get_at ds2 (len2 - 1 - i) else '0' in
+                let num1 = digit_of_char char1 in
+                let num2 = digit_of_char char2 in
+                let (current_sum, new_carry) = add num1 num2 carry in
+                let new_acc = Char.chr (current_sum + 48) :: acc in 
+                iterate (i + 1) new_carry new_acc
+            else 
+                process_list acc
+        in
+
+        iterate 0 0 []
     in
 
-    string_make_fwork (fun fwork -> iterate ds1 ds2 0 0 fwork)
+    string_make_fwork string_of_result
+
