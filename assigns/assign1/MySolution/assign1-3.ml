c16e7a40785039a1a5e7dc548cbd8141998a2043
Naomi Downing <downing3@bu.edu>
2023-09-20 17:47:13 -0400

    final version

---
+++ assigns/assign1/MySolution/assign1-3.ml
@@ -0,0 +1,18 @@
+let string_avoid_132(cs: string): bool = 
+  let len = String.length cs in
+
+  let rec helper index =
+    if index + 2 >= len then true
+    else
+      let first = cs.[index] in
+      let second = cs.[index + 1] in
+      let third = cs.[index + 2] in
+      if first < third && third < second then false
+      else helper (index + 1)
+  in
+
+  helper 0;;
+
+
+
+
