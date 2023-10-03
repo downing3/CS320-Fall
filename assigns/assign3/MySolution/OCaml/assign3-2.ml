7b8654e9bc9eec90ba74900cccd55f774d19d9e2
Naomi Downing <downing3@bu.edu>
2023-10-03 17:29:20 -0400

    added ocaml library

--- assigns/assign3/MySolution/OCaml/assign3-2.ml
+++ assigns/assign3/MySolution/OCaml/assign3-2.ml
@@ -1,30 +1,19 @@
 #use "../../../../classlib/OCaml/MyOCaml.ml";;
 
-let list_map = fun xs -> foreach_to_map_list(list_foreach)(xs)
-
-
 let len l = List.fold_left (fun acc _ -> acc + 1) 0 l;;
 
 let list_subsets (xs: 'a list): 'a list list =
   let list_length = len xs in
   if list_length = 0 then [[]]
-  else if list_length = 1 then 
-    match xs with
-    | [x] -> [[x]; []]
-    | _ -> failwith "Unexpected list length" 
-  else if list_length = 2 then 
-    match xs with
-    | [x; y] -> [[x;y]; [x]; [y]; []]
-    | _ -> failwith "Unexpected list length" 
+  else if list_length = 1 then [xs]
+  else if list_length = 2 then [xs]
   else
     let base = [[]] in
+
     List.fold_left (fun acc x ->
         acc @ (List.map (fun subset -> x :: subset) acc)
     ) base xs
 ;;
-
-
-
 
 
 
