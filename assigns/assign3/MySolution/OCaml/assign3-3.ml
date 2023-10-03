24e58775f58701279135d17f1b8a78e020633645
Naomi Downing <downing3@bu.edu>
2023-10-03 17:26:51 -0400

    added ocaml library

--- assigns/assign3/MySolution/OCaml/assign3-3.ml
+++ assigns/assign3/MySolution/OCaml/assign3-3.ml
@@ -1,22 +1,5 @@
-(*
-Assign3-3:
-HX-2023-09-26: 10 points
-//
-The function [list_nchoose(xs)(n0)]
-returns all the subsequences of xs that are
-of length n0.
-//
-let rec
-list_nchoose
-(xs: 'a list)(n0: int): 'a list list =
-//
-Please give a NON-RECURSIVE implementation of
-list_nchoose based on list-combinators. Note that
-the order of the elements in a list representation
-of a subsequenc is SIGNIFICANT. For instance, [1;2]
-and [2;1] are DIFFERENT.
-//
-*)
+#use "../../../../classlib/OCaml/MyOCaml.ml";;
+
 let len l = List.fold_left (fun acc _ -> acc + 1) 0 l;;
 
 let list_nchoose (xs: 'a list) (n0: int): 'a list list =
