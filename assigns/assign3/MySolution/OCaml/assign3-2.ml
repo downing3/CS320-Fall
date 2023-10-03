815120c6c323e2dc84eec35f3af0b5808537fe56
Naomi Downing <downing3@bu.edu>
2023-10-03 17:26:45 -0400

    added ocaml library

--- assigns/assign3/MySolution/OCaml/assign3-2.ml
+++ assigns/assign3/MySolution/OCaml/assign3-2.ml
@@ -1,38 +1,31 @@
-(*
-Assign3-2:
-HX-2023-09-26: 10 points
-//
-The function [list_subsets]
-returns all the subsets of a given
-set (where sets are represented as lists)
-//
-let rec list_subsets (xs: 'a list): 'a list list =
-(match xs with
-  [] -> [[]]
-| x1 :: xs ->
-  let res = list_subsets(xs) in
-    res @ list_map(res, fun(xs) -> x1 :: xs)
-)
-//
-Please give a NON-RECURSIVE implementation of
-list_subsets based on list-combinators. Note that
-the order of the elements in a list representation
-of a set is insignificant. For instance, [1,2] and
-[2,1] represents the same set {1,2}.
-//
-*)
+#use "../../../../classlib/OCaml/MyOCaml.ml";;
+
+let list_map = fun xs -> foreach_to_map_list(list_foreach)(xs)
+
 
 let len l = List.fold_left (fun acc _ -> acc + 1) 0 l;;
 
 let list_subsets (xs: 'a list): 'a list list =
   let list_length = len xs in
   if list_length = 0 then [[]]
-  else if list_length = 1 then [xs]
-  else if list_length = 2 then [xs]
+  else if list_length = 1 then 
+    match xs with
+    | [x] -> [[x]; []]
+    | _ -> failwith "Unexpected list length" 
+  else if list_length = 2 then 
+    match xs with
+    | [x; y] -> [[x;y]; [x]; [y]; []]
+    | _ -> failwith "Unexpected list length" 
   else
+    let base = [[]] in
     List.fold_left (fun acc x ->
         acc @ (List.map (fun subset -> x :: subset) acc)
-    ) [[]] xs
+    ) base xs
+;;
+
+
+
+
 
 
 
