6f7a26ec3f00828292e135bf3e84f1907bea9729
Naomi Downing <downing3@bu.edu>
2023-10-05 20:23:43 -0400

    uploading answer

--- exams/quiz2/MySolution/OCaml/quiz2-6.ml
+++ exams/quiz2/MySolution/OCaml/quiz2-6.ml
@@ -11,7 +11,8 @@ Please give an implementation of list_reverse based on list_foldright
 (* ************************************************ *)
 
 let list_reverse (xs: 'a list): 'a list =
-  let f x acc = acc @ [x] in
-  list_foldright f xs []
+    let f x acc = x :: acc in
+    list_foldright f xs []
+  
 
 
