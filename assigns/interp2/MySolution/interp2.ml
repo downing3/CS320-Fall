9eb91183d68f8b4edc4535bbf5c2914fdb8dd2ad
Naomi Downing <downing3@bu.edu>
2023-12-04 19:22:50 -0500

    fixing implementations

--- assigns/interp2/MySolution/interp2.ml
+++ assigns/interp2/MySolution/interp2.ml
@@ -1,198 +1,191 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
 type symbol = 
-   | Chr of char 
-   | Dig of symbol * char
+   | Char of char 
+   | CDig of symbol * char
 
-(* Abstract syntax tree for constants *)
 type const =
-  | Int of int
-  | Bool of bool
-  | Unit
-  | Symbol of symbol
+   | Int of int
+   | Bool of bool
+   | Unit
+   | Sym of symbol
+
+type com =
+   | Push of const | Pop | Swap | Trace
+   | Add | Sub | Mul | Div
+   | And | Or | Not 
+   | Lt | Gt
+   | If of coms * coms
+   | Bind | Lookup 
+   | Fun of coms | Call | Return
 
 and coms = com list
 
-(* Type definition for values *)
-and value =
-  | Const of const
-  | Closure of closure
+type value =
+    | Const of const 
+    | Closure of closure
 
-and environment = (string * value) list
+and enviornment = (string * value) list
 
-and closure = {
-   name: string;
-   environ : environment;
-   body: coms;
-}
+and closure = symbol * enviornment * coms
 
-(* Extended command types *)
-and com = 
-  | Push of const
-  | Pop
-  | Swap
-  | Trace
-  | Add | Sub | Mul | Div
-  | And | Or | Not
-  | Lt | Gt
-  | If of coms * coms
-  | Else
-  | End
-  | Bind 
-  | Lookup
-  | Fun of coms
-  | Call
-  | Return
+type stack = value list 
 
-type eval_err =
-  | StackUnderflow
-  | NonSymbolArgumentForBind
-  | NonClosureArgumentForBind
-  | UnboundSymbol of string
-  | NonSymbolValueForBind
-  | NonClosureArgumentForCall
-  | NonClosureValueForReturn
-  | Panic
+type trace = string list 
 
-exception EvalError of eval_err
-
-type call_stack = (environment * coms) list
-
-
-(* Helper function to convert a list of characters to a string *)
-let rec string_of_chars chars =
-  match chars with
-  | [] -> ""
-  | c :: cs -> String.make 1 c ^ string_of_chars cs
-
-let rec custom_assoc x env =
-  match env with
-  | [] -> None
-  | (sym, value) :: rest ->
-    if sym = x then Some value else custom_assoc x rest
-
-let rec choice parsers input =
-  match parsers with
-  | [] -> None
-  | parser :: rest -> 
-      match parser input with
-      | Some _ as result -> result
-      | None -> choice rest input
-
-(* parsers for interp1 *)
-let parse_nat = 
-  let* n = natural << whitespaces in pure n
-
-let parse_int =
-  (let* n = parse_nat in pure (Int n)) <|>
-  (keyword "-" >> let* n = parse_nat in pure (Int (-n)))
-
-let parse_bool =
-  (keyword "True" >> pure (Bool true)) <|>
-  (keyword "False" >> pure (Bool false))
-
-let parse_unit =
-  keyword "Unit" >> pure Unit
-
-(*ALL PARSERS*)
-
-(* Parser for a single character *)
-let parse_char =
-  satisfy (fun c -> ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-
-(* Parser for a digit character *)
-let parse_digit_char =
-  satisfy (fun c -> '0' <= c && c <= '9')
-
-(* Parser for a symbol *)
-let parse_symbol =
-  let rec build_sym acc =
-    (parse_char >>= fun c -> build_sym (Dig (acc, c))) <|>
-    (parse_digit_char >>= fun d -> build_sym (Dig (acc, d))) <|>
-    pure acc
-  in
-  parse_char >>= fun c -> build_sym (Chr c) >>= fun sym -> pure (Symbol sym)
-
-(* Parser for constants, with symbol parsing attempted last *)
-let parse_const =
-  parse_int <|>
-  parse_bool <|>
-  parse_unit <|>
-  parse_symbol  (* Ensure symbol parsing is the last option *)
-
-(* Parser for commands *)
-let rec parse_com () =
-  let* _ = pure () in  
-  choice [
-    (keyword "Push" >> parse_const >>= fun c -> pure (Push c));
-    (keyword "Pop" >> pure Pop);
-    (keyword "Swap" >> pure Swap);
-    (keyword "Trace" >> pure Trace);
-    (keyword "Add" >> pure Add);
-    (keyword "Sub" >> pure Sub);
-    (keyword "Mul" >> pure Mul);
-    (keyword "Div" >> pure Div);
-    (keyword "And" >> pure And);
-    (keyword "Or" >> pure Or);
-    (keyword "Not" >> pure Not);
-    (keyword "Lt" >> pure Lt);
-    (keyword "Gt" >> pure Gt);
-    (keyword "If" >> (whitespaces >> parse_coms ()) >>= fun if_branch ->
-      keyword "Else" >> (whitespaces >> parse_coms ()) >>= fun else_branch ->
-      keyword "End" >> pure (If (if_branch, else_branch)));
-    (keyword "Bind" >> pure Bind);
-    (keyword "Lookup" >> pure Lookup);
-    (keyword "Fun" >> (whitespaces >> parse_coms ()) >>= fun body ->
-      keyword "End" >> pure (Fun body));
-    (keyword "Call" >> pure Call);
-    (keyword "Return" >> pure Return)
-  ]
-
-
-(* Parser for a sequence of commands *)
-and parse_coms () =
-  let* _ = pure () in  
-  many (parse_com () << keyword ";")
-
-
-(* interpreter *)
-
-type stack = value list
-type trace = string list
 type prog = coms
 
-let rec str_of_nat (n : int) : string =
-  let d = n mod 10 in 
-  let n0 = n / 10 in
-  let s = str (chr (d + ord '0')) in 
-  if 0 < n0 then
-    string_append (str_of_nat n0) s
-  else s
 
-let str_of_int (n : int) : string = 
-  if n < 0 then
-    string_append "-" (str_of_nat (-n))
-  else str_of_nat n
+(* HELPER FUNCTIONS *)
 
-let rec string_of_symbol sym =
-  match sym with
-  | Chr c -> String.make 1 c
-  | Dig (s, c) -> string_of_symbol s ^ String.make 1 c
+let is_lower_case c =
+   'a' <= c && c <= 'z'
 
-let rec toString c =
-  match c with
-  | Int i -> str_of_int i
-  | Bool b -> if b then "True" else "False"
-  | Unit -> "Unit"
-  | Symbol s -> string_of_symbol s
+let is_upper_case c =
+   'A' <= c && c <= 'Z'
 
-let rec retrieve sym env =
+let is_alpha c =
+   is_lower_case c || is_upper_case c
+
+let is_digit c =
+   '0' <= c && c <= '9'
+
+let rec lookup_env (name: string) (env: enviornment) : value =
   match env with
-  | [] -> None
-  | (key, value) :: rest ->
-    if key = sym then Some value else retrieve sym rest
+  | (x, v_value) :: rest ->
+    if name = x then v_value 
+    else lookup_env name rest
+  | [] -> failwith "Variable not found in environment"
 
-let rec eval (s : stack) (t : trace) (v : environment) (p : prog) : trace =
+let rec str_of_nat(n: int): string = 
+   let d = n mod 10 in 
+   let n0 = n / 10 in
+   let s = str (chr(d + ord '0')) in 
+   if 0 < n0 then
+      string_append(str_of_nat n0) s
+   else 
+      s
+
+let str_of_int(n: int): string = 
+   if n < 0 then
+      string_append "-" (str_of_nat (-n))
+   else 
+      str_of_nat n
+
+let rec str_of_sym(s: symbol): string =
+   match s with 
+   | Char c -> str c
+   | CDig (s1, c1) -> string_append (str_of_sym s1) (str c1)
+
+let toString(c: const): string =
+   match c with 
+   | Int i -> str_of_int i
+   | Bool true -> "True"
+   | Bool false -> "False"
+   | Unit -> "Unit"
+   | Sym x -> str_of_sym x
+
+let nat_parser = 
+   let* n = natural << whitespaces in 
+      pure n
+
+let int_parser =
+   (let* n = nat_parser in 
+      pure (Int n))
+   <|>
+   (keyword "-" >> let* n = nat_parser in 
+      pure (Int (-n)))
+
+let bool_parser =
+   (keyword "True" >> pure (Bool true)) 
+   <|> 
+   (keyword "False" >> pure (Bool false))
+
+let unit_parser =
+   keyword "Unit" >> pure Unit
+
+let char_parser =
+   satisfy is_alpha
+
+let digit_parser = 
+   satisfy is_digit
+
+let rec sym_parser =
+  let char_sym_parser =
+    char_parser >>= fun c -> pure (Char c)
+  in
+
+  let rec concat_sym_parser acc =
+    (char_parser >>= fun c ->
+      concat_sym_parser (CDig (acc, c)))
+    <|>
+    (digit_parser >>= fun d ->
+      concat_sym_parser (CDig (acc, d)))
+    <|>
+    pure acc
+  in
+
+  char_sym_parser >>= concat_sym_parser
+
+
+let const_parser =
+   int_parser
+   <|>
+   bool_parser
+   <|>
+   unit_parser
+   <|>
+   (sym_parser >>= fun s ->
+      pure (Sym s))
+
+let rec com_parser input =
+   ((keyword "Push" >> const_parser >>= fun c ->
+      pure (Push c))
+   <|>
+   (keyword "Pop" >> pure Pop)
+   <|>
+   (keyword "Swap" >> pure Swap)
+   <|>
+   (keyword "Trace" >> pure Trace)
+   <|>
+   (keyword "Add" >> pure Add)
+   <|>
+   (keyword "Sub" >> pure Sub)
+   <|>
+   (keyword "Mul" >> pure Mul)
+   <|>
+   (keyword "Div" >> pure Div)
+   <|>
+   (keyword "And" >> pure And)
+   <|>
+   (keyword "Or" >> pure Or)
+   <|>
+   (keyword "Not" >> pure Not)
+   <|>
+   (keyword "Lt" >> pure Lt)
+   <|>
+   (keyword "Gt" >> pure Gt)
+   <|>
+   (keyword "If" >> coms_parser >>= fun c1 ->
+      keyword "Else" >> coms_parser >>= fun c2 ->
+      keyword "End" >> pure (If (c1, c2)))
+   <|> 
+   (keyword "Bind" >> pure Bind)
+   <|>
+   (keyword "Lookup" >> pure Lookup)
+   <|>
+   (keyword "Fun" >> coms_parser >>= fun c ->
+      keyword "End" >> pure (Fun c))
+   <|>
+   (keyword "Call" >> pure Call)
+   <|>
+   (keyword "Return" >> pure Return)) input
+
+and coms_parser input =   
+   (many (com_parser << keyword ";")) input
+
+
+let rec eval (s : stack) (t : trace) (v : enviornment) (p : prog) : trace =
   match p with
   | [] -> t
   | Push c :: p0 -> eval (Const c :: s) t v p0
@@ -251,69 +244,61 @@ let rec eval (s : stack) (t : trace) (v : environment) (p : prog) : trace =
         let branch = if b then c1 else c2 in
         eval s0 t v (branch @ p0)
     | _ -> eval [] ("Panic" :: t) v [])
-  | Bind :: p0 ->
-    (match s with
-    | Const (Symbol x) :: v0 :: s0 ->
-        let sym_str = string_of_symbol x in
-        let new_env = (sym_str, v0) :: v in
-        eval s0 t new_env p0
-    | _ -> eval [] ("Panic" :: t) v [])
+   | Bind :: p0 -> 
+  (match s with 
+  | Const (Sym x) :: value :: s0 -> 
+    eval s0 t ((str_of_sym x, value) :: v) p0 
+  | _ :: value :: s0 -> eval [] ("Panic" :: t) v []
+  | _ :: s0 -> eval [] ("Panic" :: t) v []
+  | [] -> eval [] ("Panic" :: t) v [])
   | Lookup :: p0 ->
-    (match s with
-    | Const (Symbol x) :: s0 ->
-        (match custom_assoc (string_of_symbol x) v with
-        | Some v0 -> eval (v0 :: s0) t v p0
-        | None -> eval [] ("Panic" :: t) v [])
-    | _ -> eval [] ("Panic" :: t) v [])
-  | Fun cmd :: End :: p0 ->
-    (match s with
-    | Const (Symbol x) :: s0 ->
-      let closure = { name = string_of_symbol x; environ = v; body = cmd } in
-      eval (Closure closure :: s0) t v p0
-    | _ -> eval [] ("Panic" :: t) v [])
-  | Call :: p0 ->
-    (match s with
-    | Const (Symbol f) :: a :: s0 ->
-        (match custom_assoc (string_of_symbol f) v with
-        | Some (Closure closure) ->
-            let updated_env = (closure.name, Closure closure) :: closure.environ in
-            let cc = Symbol (Chr 'c') in
-            let new_continuation = Closure { name = ""; environ = v; body = p0 } in
-            let new_stack = a :: new_continuation :: s0 in
-            eval (new_stack) t updated_env closure.body
-        | _ -> eval [] ("Panic" :: t) v [])
-    | _ -> eval [] ("Panic" :: t) v [])
-  | Return :: p0 ->
-    (match s with
-    | Const (Symbol f) :: a :: s0 ->
-        (match custom_assoc (string_of_symbol f) v with
-        | Some (Closure closure) ->
-            eval (a :: s0) t closure.environ closure.body
-        | _ -> eval [] ("Panic" :: t) v [])
-    | _ -> eval [] ("Panic" :: t) v [])
+    (match s with 
+    | Const (Sym x) :: s0 -> 
+      let found = lookup_env (str_of_sym x) v in 
+      eval (found :: s0) t v p0
+    | _ :: s0 -> eval [] ("Panic" :: t) v []
+    | [] -> eval [] ("Panic" :: t) v [])
 
+  | Fun c :: p0 ->
+    (match s with 
+    | Const (Sym x) :: s0 -> 
+      eval (Closure (x, v, c) :: s0) t v p0
+    | _ :: s0 -> eval [] ("Panic" :: t) v []
+    | [] -> eval [] ("Panic" :: t) v [])
 
+  | Call :: p0 -> 
+    (match s with
+    | Closure (f, vf, c) :: a :: s0 ->
+      eval (a :: Closure (f, v, p0) :: s0) t ((str_of_sym f, Closure (f, vf, c)) :: vf) c
+    | _ :: a :: s0 -> eval [] ("Panic" :: t) v []
+    | _ :: s0 -> eval [] ("Panic" :: t) v []
+    | [] -> eval [] ("Panic" :: t) v [])
 
-(* putting it all together [input -> parser -> eval -> output] *)
+  | Return :: p0 -> 
+    (match s with 
+    | Closure (f, vf, c) :: a :: s0 ->
+      eval (a :: s0) t vf c
+    | _ :: a :: s0 -> eval [] ("Panic" :: t) v []
+    | _ :: s0 -> eval [] ("Panic" :: t) v []
+    | [] -> eval [] ("Panic" :: t) v [])
 
-let interp (s : string) : string list option =
-  match string_parse (whitespaces >> parse_coms()) s with
+let interp (s: string): string list option =
+   match string_parse (whitespaces >> coms_parser) s with
   | Some (p, []) -> Some (eval [] [] [] p)
   | _ -> None
 
-(* interp from file *)
 
-let read_file (fname : string) : string =
-  let fp = open_in fname in
-  let s = string_make_fwork (fun work ->
+let read_file (fname: string): string =
+   let fp = open_in fname in
+   let s = string_make_fwork (fun work ->
       try
-        while true do
-          work (input_char fp)
-        done
+         while true do
+            work (input_char fp)
+         done
       with _ -> ())
-  in
-  close_in fp; s
+   in
+   close_in fp; s
 
-let interp_file (fname : string) : string list option =
-  let src = read_file fname in
-  interp src
+let interp_file (fname: string): string list option =
+   let src = read_file fname in
+   interp src
---
+++ assigns/interp2/MySolution/test.ml
@@ -0,0 +1,364 @@
+(* ****** ****** *)
+
+#use "./../../../classlib/OCaml/MyOCaml.ml";;
+
+
+(* GRAMMAR *)
+
+(* 
+grammar: constants 
+
+⟨digit⟩ ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+⟨nat⟩ ::= ⟨digit⟩ | ⟨digit⟩⟨nat⟩
+⟨int⟩ ::= ⟨nat⟩ | -⟨nat⟩
+⟨bool⟩ ::= True | False
+⟨char⟩ ::= a | b | ... | z
+⟨sym⟩ ::= ⟨char⟩ | ⟨sym⟩⟨char⟩ | ⟨sym⟩⟨digit⟩
+⟨const⟩ ::= ⟨int⟩ | ⟨bool⟩ | Unit | ⟨sym⟩
+*)
+
+(* 
+grammar: programs 
+
+⟨prog⟩ ::= ⟨coms⟩
+⟨com⟩ ::= Push ⟨const⟩ | Pop | Swap | Trace
+          | Add | Sub | Mul | Div
+          | And | Or | Not
+          | Lt | Gt
+          | If ⟨coms⟩ Else ⟨coms⟩ End
+          | Bind | Lookup
+          | Fun ⟨coms⟩ End | Call | Return
+⟨coms⟩ ::= ϵ | ⟨com⟩; ⟨coms⟩
+*)
+
+
+(* TYPE DEFINITIONS *)
+
+type sym = 
+   | Char of char 
+   | CDig of sym * char
+
+type const =
+   | Int of int
+   | Bool of bool
+   | Unit
+   | Sym of sym
+
+type com =
+   | Push of const | Pop | Swap | Trace
+   | Add | Sub | Mul | Div
+   | And | Or | Not 
+   | Lt | Gt
+   | If of coms * coms
+   | Bind | Lookup 
+   | Fun of coms | Call | Return
+
+and coms = com list
+
+type value =
+    | Const of const 
+    | Closure of closure
+
+and venv = (string * value) list
+
+and closure = {
+   name: sym;
+   capt_env: venv;
+   body: coms;
+}
+
+type stack = value list 
+
+type trace = string list 
+
+type prog = coms
+
+
+(* HELPER FUNCTIONS *)
+
+let is_lower_case c =
+   'a' <= c && c <= 'z'
+
+let is_upper_case c =
+   'A' <= c && c <= 'Z'
+
+let is_alpha c =
+   is_lower_case c || is_upper_case c
+
+let is_digit c =
+   '0' <= c && c <= '9'
+
+let rec fetch (s: string) (v: venv) =
+   match v with
+   | (x, v0) :: v1 -> 
+      if s = x then 
+         v0 
+      else 
+         fetch s v1
+
+
+(* STRING FUNCTIONS *)
+
+let rec str_of_nat(n: int): string = 
+   let d = n mod 10 in 
+   let n0 = n / 10 in
+   let s = str (chr(d + ord '0')) in 
+   if 0 < n0 then
+      string_append(str_of_nat n0) s
+   else 
+      s
+
+let str_of_int(n: int): string = 
+   if n < 0 then
+      string_append "-" (str_of_nat (-n))
+   else 
+      str_of_nat n
+
+let rec str_of_sym(s: sym): string =
+   match s with 
+   | Char c -> str c
+   | CDig (s1, c1) -> string_append (str_of_sym s1) (str c1)
+
+let toString(c: const): string =
+   match c with 
+   | Int i -> str_of_int i
+   | Bool true -> "True"
+   | Bool false -> "False"
+   | Unit -> "Unit"
+   | Sym x -> str_of_sym x
+
+
+(* PARSERS *)
+
+let nat_parser = 
+   let* n = natural << whitespaces in 
+      pure n
+
+let int_parser =
+   (let* n = nat_parser in 
+      pure (Int n))
+   <|>
+   (keyword "-" >> let* n = nat_parser in 
+      pure (Int (-n)))
+
+let bool_parser =
+   (keyword "True" >> pure (Bool true)) 
+   <|> 
+   (keyword "False" >> pure (Bool false))
+
+let unit_parser =
+   keyword "Unit" >> pure Unit
+
+let char_parser =
+   satisfy is_alpha
+
+let digit_parser = 
+   satisfy is_digit
+
+let rec sym_parser =
+  let char_sym_parser =
+    char_parser >>= fun c -> pure (Char c)
+  in
+
+  let rec concat_sym_parser acc =
+    (char_parser >>= fun c ->
+      concat_sym_parser (CDig (acc, c)))
+    <|>
+    (digit_parser >>= fun d ->
+      concat_sym_parser (CDig (acc, d)))
+    <|>
+    pure acc
+  in
+
+  char_sym_parser >>= concat_sym_parser
+
+
+let const_parser =
+   int_parser
+   <|>
+   bool_parser
+   <|>
+   unit_parser
+   <|>
+   (sym_parser >>= fun s ->
+      pure (Sym s))
+
+let rec com_parser input =
+   ((keyword "Push" >> const_parser >>= fun c ->
+      pure (Push c))
+   <|>
+   (keyword "Pop" >> pure Pop)
+   <|>
+   (keyword "Swap" >> pure Swap)
+   <|>
+   (keyword "Trace" >> pure Trace)
+   <|>
+   (keyword "Add" >> pure Add)
+   <|>
+   (keyword "Sub" >> pure Sub)
+   <|>
+   (keyword "Mul" >> pure Mul)
+   <|>
+   (keyword "Div" >> pure Div)
+   <|>
+   (keyword "And" >> pure And)
+   <|>
+   (keyword "Or" >> pure Or)
+   <|>
+   (keyword "Not" >> pure Not)
+   <|>
+   (keyword "Lt" >> pure Lt)
+   <|>
+   (keyword "Gt" >> pure Gt)
+   <|>
+   (keyword "If" >> coms_parser >>= fun c1 ->
+      keyword "Else" >> coms_parser >>= fun c2 ->
+      keyword "End" >> pure (If (c1, c2)))
+   <|> 
+   (keyword "Bind" >> pure Bind)
+   <|>
+   (keyword "Lookup" >> pure Lookup)
+   <|>
+   (keyword "Fun" >> coms_parser >>= fun c ->
+      keyword "End" >> pure (Fun c))
+   <|>
+   (keyword "Call" >> pure Call)
+   <|>
+   (keyword "Return" >> pure Return)) input
+
+and coms_parser input =   
+   (many (com_parser << keyword ";")) input
+
+
+(* INTERPRETER FUNCTIONS *)
+
+
+let rec eval_step(s: stack)(t: trace)(v: venv)(p: prog): trace =
+   match p with
+   | [] -> t
+   | Push c :: p0 -> eval_step (Const c :: s) t v p0
+   | Pop :: p0 -> 
+      (match s with 
+      | _ :: s0 -> eval_step s0 t v p0
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Swap :: p0 -> 
+      (match s with 
+      | c1 :: c2 :: s0 -> eval_step (c2 :: c1 :: s0) t v p0
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | Trace :: p0 -> 
+      (match s with
+      | Const c :: s0 -> eval_step (Const Unit :: s0) (toString c :: t) v p0
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Add :: p0 -> 
+      (match s with
+      | Const(Int i) :: Const(Int j) :: s0 -> eval_step (Const(Int (i + j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | Sub :: p0 -> 
+      (match s with
+      | Const(Int i) :: Const(Int j) :: s0 -> eval_step (Const(Int (i - j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+  | Mul :: p0 -> 
+      (match s with
+      | Const(Int i) :: Const(Int j) :: s0 -> eval_step (Const(Int (i * j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+  | Div :: p0 -> 
+      (match s with
+      | Const(Int i) :: Const(Int 0) :: s0 -> eval_step [] ("Panic" :: t) v []
+      | Const(Int i) :: Const(Int j) :: s0 -> eval_step (Const(Int (i / j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | And :: p0 -> 
+      (match s with
+      | Const(Bool a) :: Const(Bool b) :: s0 -> eval_step (Const(Bool (a && b)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+  | Or :: p0 -> 
+      (match s with
+      | Const(Bool a) :: Const(Bool b) :: s0 -> eval_step (Const(Bool (a || b)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | Not :: p0 -> 
+      (match s with
+      | Const(Bool a) :: s0 -> eval_step (Const(Bool (not a)) :: s0) t v p0
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | Lt :: p0 -> 
+      (match s with
+      | Const(Int i) :: Const(Int j) :: s0 -> eval_step (Const(Bool (i < j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+  | Gt :: p0 -> 
+      (match s with
+      | Const (Int i) :: Const (Int j) :: s0 -> eval_step (Const(Bool (i > j)) :: s0) t v p0
+      | _ :: _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v []
+      | _ :: [] -> eval_step [] ("Panic" :: t) v [])
+   | If (c1, c2) :: p0 -> 
+      (match s with 
+      | Const(Bool b) :: s0 -> 
+         if b = true then 
+            eval_step s0 t v (c1 @ p0)
+         else 
+            eval_step s0 t v (c2 @ p0)
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Bind :: p0 -> 
+      (match s with 
+      | Const (Sym x) :: v0 :: s0 -> 
+         eval_step s0 t ((str_of_sym x, v0) :: v) p0 
+      | _ :: v0 :: s0 -> eval_step [] ("Panic" :: t) v []
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Lookup :: p0 ->
+      (match s with 
+      | Const (Sym x) :: s0 -> 
+         let found = fetch (str_of_sym x) v in 
+         eval_step (found :: s0) t v p0
+         (*
+         (match v with 
+         | (x, v0) :: v1 -> eval_step (v0 :: s0) t v p0
+         | _ :: v0 -> eval_step [] ("Panic" :: t) v [])*)
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Fun c :: p0 ->
+      (match s with 
+      | Const (Sym x) :: s0 -> 
+         let f = {name = x; capt_env = v; body = c} in 
+         eval_step (Closure f :: s0) t v p0
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v [])
+   | Call :: p0 -> 
+      (match s with
+      | Const (Sym f) :: a :: s0 ->
+        let closure = fetch (str_of_sym f) v in 
+        eval_step (a :: Const (Sym f) :: s0) t ((str_of_sym f, closure) :: v) p0
+      | _ :: a :: s0 -> eval_step [] ("Panic" :: t) v []
+      | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+      | [] -> eval_step [] ("Panic" :: t) v [])
+    | Return :: p0 -> 
+        (match s with 
+        | Const (Sym f) :: a :: s0 ->
+            (match fetch (str_of_sym f) v with
+            | Closure closure ->
+                eval_step (a :: s0) t closure.capt_env closure.body
+            | _ -> eval_step [] ("Panic" :: t) v [])
+        | _ :: a :: s0 -> eval_step [] ("Panic" :: t) v []
+        | _ :: s0 -> eval_step [] ("Panic" :: t) v []
+        | [] -> eval_step [] ("Panic" :: t) v [])
+
+let interp (s: string): string list option =
+   match string_parse (whitespaces >> coms_parser) s with
+  | Some (p, []) -> Some (eval_step [] [] [] p)
+  | _ -> None
